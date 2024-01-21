4973c650133a63422b4308e59ec2fe58c926e4d4
yaroslavDp <bayern744@gmail.com>
2024-01-21 20:03:30 +0200

    Fix_4

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7aiwggu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(8080);
+    app.listen(process.env.PORT || 8080);
   })
   .catch((err) => {
     console.log(err);
